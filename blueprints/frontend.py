83c6bdf8f55ed9d4d03e8850c74414c9330eb346
root <root@seven27.europe-central2-a.c.seventwentyseven-324019.internal>
2021-10-26 14:55:54 +0000

    Our changes

--- blueprints/frontend.py
+++ blueprints/frontend.py
@@ -9,6 +9,7 @@ import time
 
 from cmyui.logging import Ansi
 from cmyui.logging import log
+from cmyui.osu import Mods
 from functools import wraps
 from PIL import Image
 from pathlib import Path
@@ -42,7 +43,66 @@ def login_required(func):
 @frontend.route('/home')
 @frontend.route('/')
 async def home():
-    return await render_template('home.html')
+    record = {}
+    record['std-vn'] = await glob.db.fetch("SELECT scores_vn.id, scores_vn.pp, scores_vn.userid, "
+                                           "maps.set_id, users.name FROM scores_vn LEFT JOIN users ON "
+                                           "scores_vn.userid = users.id LEFT JOIN maps ON scores_vn.map_md5 "
+                                           "= maps.md5 WHERE scores_vn.mode = 0 && maps.status=2 "
+                                           "&& users.priv & 1 ORDER BY pp DESC LIMIT 1;")
+
+    record['std-rx'] = await glob.db.fetch("SELECT scores_rx.id, scores_rx.pp, scores_rx.userid, "
+                                           "maps.set_id, users.name FROM scores_rx LEFT JOIN users ON "
+                                           "scores_rx.userid = users.id LEFT JOIN maps ON scores_rx.map_md5 "
+                                           "= maps.md5 WHERE scores_rx.mode = 0 && maps.status=2 "
+                                           "&& users.priv & 1 ORDER BY pp DESC LIMIT 1;")
+
+    record['std-ap'] = await glob.db.fetch("SELECT scores_ap.id, scores_ap.pp, scores_ap.userid, "
+                                           "maps.set_id, users.name FROM scores_ap LEFT JOIN users ON "
+                                           "scores_ap.userid = users.id LEFT JOIN maps ON scores_ap.map_md5 "
+                                           "= maps.md5 WHERE scores_ap.mode = 0 && maps.status=2 "
+                                           "&& users.priv & 1 ORDER BY pp DESC LIMIT 1;")
+
+    record['taiko-vn'] = await glob.db.fetch("SELECT scores_vn.id, scores_vn.pp, scores_vn.userid, "
+                                           "maps.set_id, users.name FROM scores_vn LEFT JOIN users ON "
+                                           "scores_vn.userid = users.id LEFT JOIN maps ON scores_vn.map_md5 "
+                                           "= maps.md5 WHERE scores_vn.mode = 1 && maps.status=2 "
+                                           "&& users.priv & 1 ORDER BY pp DESC LIMIT 1;")
+
+    record['taiko-rx'] = await glob.db.fetch("SELECT scores_rx.id, scores_rx.pp, scores_rx.userid, "
+                                           "maps.set_id, users.name FROM scores_rx LEFT JOIN users ON "
+                                           "scores_rx.userid = users.id LEFT JOIN maps ON scores_rx.map_md5 "
+                                           "= maps.md5 WHERE scores_rx.mode = 1 && maps.status=2 "
+                                           "&& users.priv & 1 ORDER BY pp DESC LIMIT 1;")
+
+    record['catch-vn'] = await glob.db.fetch("SELECT scores_vn.id, scores_vn.pp, scores_vn.userid, "
+                                           "maps.set_id, users.name FROM scores_vn LEFT JOIN users ON "
+                                           "scores_vn.userid = users.id LEFT JOIN maps ON scores_vn.map_md5 "
+                                           "= maps.md5 WHERE scores_vn.mode = 2 && maps.status=2 "
+                                           "&& users.priv & 1 ORDER BY pp DESC LIMIT 1;")
+
+    record['catch-rx'] = await glob.db.fetch("SELECT scores_rx.id, scores_rx.pp, scores_rx.userid, "
+                                           "maps.set_id, users.name FROM scores_rx LEFT JOIN users ON "
+                                           "scores_rx.userid = users.id LEFT JOIN maps ON scores_rx.map_md5 "
+                                           "= maps.md5 WHERE scores_rx.mode = 2 && maps.status=2 "
+                                           "&& users.priv & 1 ORDER BY pp DESC LIMIT 1;")
+
+    record['mania-vn'] = await glob.db.fetch("SELECT scores_vn.id, scores_vn.pp, scores_vn.userid, "
+                                           "maps.set_id, users.name FROM scores_vn LEFT JOIN users ON "
+                                           "scores_vn.userid = users.id LEFT JOIN maps ON scores_vn.map_md5 "
+                                           "= maps.md5 WHERE scores_vn.mode = 3 && maps.status=2 "
+                                           "&& users.priv & 1 ORDER BY pp DESC LIMIT 1;")
+
+    record['std-vn']['pp'] = round(float(record['std-vn']['pp']), 2)
+    record['std-rx']['pp'] = round(float(record['std-rx']['pp']), 2)
+    record['std-ap']['pp'] = round(float(record['std-ap']['pp']), 2)
+    record['taiko-vn']['pp'] = round(float(record['taiko-vn']['pp']), 2)
+    record['taiko-rx']['pp'] = round(float(record['taiko-rx']['pp']), 2)
+    record['catch-vn']['pp'] = round(float(record['catch-vn']['pp']), 2)
+    record['catch-rx']['pp'] = round(float(record['catch-rx']['pp']), 2)
+    record['mania-vn']['pp'] = round(float(record['mania-vn']['pp']), 2)
+
+    return await render_template('home.html', record=record)
+
 
 @frontend.route('/home/account/edit')
 async def home_account_edit():
@@ -304,12 +364,17 @@ async def profile_select(id):
     mode = request.args.get('mode', 'std', type=str) # 1. key 2. default value
     mods = request.args.get('mods', 'vn', type=str)
     user_data = await glob.db.fetch(
-        'SELECT name, safe_name, id, priv, country '
+        'SELECT name, safe_name, id, priv, country, clan_id '
         'FROM users '
         'WHERE safe_name IN (%s) OR id IN (%s) LIMIT 1',
         [id, utils.get_safe_name(id)]
     )
-
+    if int(user_data['clan_id']) != 0:
+        clan = await glob.db.fetch("SELECT tag FROM clans WHERE id=%s", user_data['clan_id'])
+        clan['tag'] = f"[{clan['tag']}] "
+    else:
+        clan = {}
+        clan['tag'] = ""
     # no user
     if not user_data:
         return (await render_template('404.html'), 404)
@@ -321,12 +386,39 @@ async def profile_select(id):
     if mods is not None and mods not in VALID_MODS:
         return (await render_template('404.html'), 404)
 
-    is_staff = 'authenticated' in session and session['user_data']['is_staff']
+    is_staff = 'authenticated' in session and (Privileges.Admin in Privileges(int(session['user_data']['priv'])))
     if not user_data or not (user_data['priv'] & Privileges.Normal or is_staff):
         return (await render_template('404.html'), 404)
 
+    group_list = []
+    user_priv = Privileges(int(user_data['priv']))
+    if Privileges.Normal not in user_priv:
+        group_list.append(["ban", "Restricted", "#000000"])
+    else:
+        if int(user_data['id']) in [3,4]:
+            group_list.append(["crown" ,"Owner", "#e84118"])
+        if Privileges.Dangerous in user_priv:
+            group_list.append(["code" ,"Developer", "#9b59b6"])
+        if Privileges.Admin in user_priv:
+            group_list.append(["star", "Admin", "#f39c12"])
+        if Privileges.Mod in user_priv:
+            group_list.append(["shield-alt", "GMT", "#28a40c"])
+        if Privileges.Nominator in user_priv:
+            group_list.append(["music", "BN", "#1e90ff"])
+        if Privileges.Alumni in user_priv:
+            group_list.append(["wheelchair", "Alumni", "#ea8685"])
+        if Privileges.Supporter in user_priv:
+            if Privileges.Premium in user_priv:
+                group_list.append(["gem", "Supporter+", "#f78fb3"])
+            else:
+                group_list.append(["heart", "Supporter", "#f78fb3"])
+        elif Privileges.Premium in user_priv:
+            group_list.append(["gem", "Supporter+", "#f78fb3"])
+        if Privileges.Whitelisted in user_priv:
+            group_list.append(["check", "Verified", "#28a40c"])
+    
     user_data['customisation'] = utils.has_profile_customizations(user_data['id'])
-    return await render_template('profile.html', user=user_data, mode=mode, mods=mods)
+    return await render_template('profile.html', user=user_data, mode=mode, mods=mods, group_list=group_list, clan=clan)
 
 
 @frontend.route('/leaderboard')
@@ -608,3 +700,115 @@ async def get_profile_background(user_id: int):
             return await send_file(path)
 
     return b'{"status":404}'
+
+@frontend.route('/score/<score_id>')
+@frontend.route('/score/<score_id>/<mods>')
+async def get_player_score(score_id:int=0, mods:str = "vn"):
+    if score_id == 0:
+        return await flash('error', "This score does not exist!", "home")
+    if mods.lower() not in ["vn", "rx", "ap"]:
+        return await flash('error', "Valid mods are vn, rx and ap!", "home")
+
+    # Check score
+    score = await glob.db.fetch("SELECT * FROM "
+                               f"scores_{mods.lower()} "
+                                "WHERE id=%s", score_id)
+    if not score:
+        return await flash('error', "Score not found!", "home")
+
+    # Get user
+    user = await glob.db.fetch("SELECT id, name, country, priv FROM users WHERE id=%s", score['userid'])
+    
+
+
+    if Privileges.Normal not in Privileges(int(user['priv'])):
+        if not session:
+            return (await render_template('404.html'), 404)
+        elif Privileges.Admin not in Privileges(session['user_data']['priv']):
+            return (await render_template('404.html'), 404)
+
+    #Get Map
+    map_info = await glob.db.fetch("SELECT artist, title, version AS diffname, creator, "
+                                   "diff, mode, set_id, max_combo FROM maps WHERE md5=%s", score['map_md5'])
+    if not map_info:
+        log(f"Tried fetching scoreid {score_id} in {mods} (Route: /score/): Map with md5 '{score['map_md5']}' does not exist in database,"
+        " that shouldn't happen unless you deleted it manually", Ansi.RED)
+        return await flash('error', 'Could not display score, map does not exist in database', 'home')
+    
+    #Change variables and stuff like that
+    try:
+        map_info['diff'] = round(map_info['diff'], 2)
+    except:
+        map_info['diff'] = map_info['diff']
+    score['grade'] = score['grade'].upper()
+    user['country'] = user['country'].upper()
+    user['banner'] = f"url(https://seventwentyseven.tk/banners/{user['id']});"
+    map_info['banner_link'] = f"url('https://assets.ppy.sh/beatmaps/{map_info['set_id']}/covers/cover.jpg');"
+    score['acc'] = round(float(score['acc']), 2)
+    score['pp'] = round(float(score['pp']), 2)
+    #Calculation
+    grade_colors= {
+        "F": "#ff5959",
+        "D": "#ff5959",
+        "C": "#ff56da",
+        "B": "#3d97ff",
+        "A": "#2bff35",
+        "S": "#ffcc22",
+        "SH": "#cde7e7",
+        "X": "#ffcc22",
+        "XH": "#cde7e7",
+    }
+    try:
+        grade_shadow = grade_colors[score['grade'].upper()]
+    except:
+        grade_shadow = "#FFFFFF"
+
+    grade_convert = {"XH": "SS", "X": "SS", "SH": "S"}
+    try:
+        score['grade'] = grade_convert[score['grade']]
+    except:
+        score['grade'] = score['grade']
+    #add commas to score
+    score['score'] = "{:,}".format(int(score['score']))
+    #Make badges
+    user_priv = Privileges(user['priv'])
+    group_list = []
+    if Privileges.Normal not in user_priv:
+        group_list.append(["RESTRICTED", "#FFFFFF"])
+    else:
+        if int(user['id']) in [3,4]:
+            group_list.append(["OWNER", "#e84118"])
+        if Privileges.Dangerous in user_priv:
+            group_list.append(["DEV", "#9b59b6"])
+        elif Privileges.Admin in user_priv:
+            group_list.append(["ADM", "#fbc531"])
+        elif Privileges.Mod in user_priv:
+            group_list.append(["GMT", "#28a40c"])
+        if Privileges.Nominator in user_priv:
+            group_list.append(["BN", "#1e90ff"])
+        if Privileges.Alumni in user_priv:
+            group_list.append(["ALU", "#ea8685"])
+        if Privileges.Supporter in user_priv:
+            if Privileges.Premium in user_priv:
+                group_list.append(["❤❤", "#f78fb3"])
+            else:
+                group_list.append(["❤", "#f78fb3"])
+        elif Privileges.Premium in user_priv:
+            group_list.append(["❤❤", "#f78fb3"])
+        if Privileges.Whitelisted in user_priv:
+            group_list.append(["✓", "#28a40c"])
+        
+    #Get status
+    async with glob.http.get(f"https://osu.seventwentyseven.tk/api/get_player_status?id={user['id']}") as resp:
+        resp = await resp.json()
+        if resp['player_status']['online'] == True:
+            player_status = ["#38c714", "Online"]
+        else:
+            player_status = ["#000000", "Offline"]
+    
+    #Mods
+        if int(score['mods']) != 0:
+            score['mods'] = f"+{Mods(int(score['mods']))!r}"
+    return await render_template('score.html', score=score, user=user, map_info=map_info, 
+                                grade_shadow=grade_shadow, group_list=group_list, 
+                                player_status=player_status, mode_mods=mods)
